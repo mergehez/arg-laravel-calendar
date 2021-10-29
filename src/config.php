f131be52cb794213665f5934e700bc398e10a8c9
MergeSoft <argesozdogan@gmail.com>
2021-10-29 23:21:18 +0200

    Modified and reordered config

--- src/config.php
+++ src/config.php
@@ -1,49 +1,53 @@
 <?php
 
 return [
-  'booking' => [
-    // indicates whether booking function is enabled. 
-    // TODO: implement the case when this is false
-    'enabled' => true,
+  // Indicates whether booking function is enabled. 
+  // TODO: implement the case when this is false
+  'bookingEnabled' => true,
 
-    // indicates whether a time in the past can be booked
-    'allowPast' => false,
+  // Indicates whether a time in the past can be booked
+  'allowPast' => false,
 
-    // time range options for booking.
-    'options' => [
-        "8-13" => "Morning",
-        "8-18" => "Day",
-        "8-24" => "Day + Evening",
-        "13-18" => "Afternoon",
-        "13-24" => "Afternoon + Evening",
-        "18-24" => "Evening"
+  // Indicates whether if values in the 'options' array should act like translation keys
+  'translateOptionKeys' => true,
+
+  // Time range options for booking. 
+  // Key constains start and end hours. 
+  // Value is translation key if 'translateOptionKeys' is true, otherwise the value to be printed
+  'options' => [
+      "8-13"  => "forenoon",
+      "8-18"  => "day",
+      "8-24"  => "day_evening",
+      "13-18" => "afternoon",
+      "13-24" => "afternoon_evening",
+      "18-24" => "evening"
     ],
-    
-    // If true, time spans for booking will get background colors. 
-    'showOccupation' => true,
-    'boxBackColorWhenNoOccupation' => null,
 
-    // Background color for occupied time spans
-    'occupiedBackColor' => 'var(--bs-danger)',
-    // Background color for free time spans
-    'freeBackColor' => 'var(--bs-success)',
-    // Opacity for background colors
-    'backColorOpacity' => 0.15,
+  
+  // If true, time spans for booking will get background colors. 
+  'showOccupation' => true,
+  'boxBackColorWhenNoOccupation' => null,
 
-    // Text color for today. Set to null, if you don't want to highlight
-    'todayTextColor' => 'var(--bs-primary)',
-    // Border radius for day boxes
-    'boxBorderRadius' => '0.25rem',
+  // Background color for occupied time spans
+  'occupiedBackColor' => 'var(--bs-danger)',
+  // Background color for free time spans
+  'freeBackColor' => 'var(--bs-success)',
+  // Opacity for background colors
+  'backColorOpacity' => 0.15,
 
-    // Border color for the selected day
-    'selectedBorderColor' => 'var(--bs-primary) !important',
-    // Border width for the selected day
-    'selectedBorderWidth' => '2px',
-    // Border color for the hovered day. (css hover selector)
-    'hoverBorderColor' => 'var(--bs-info)',
-    // Border width for the hovered day. (css hover selector)
-    'hoverBorderWidth' => '2px',
-  ],
+  // Text color for today. Set to null, if you don't want to highlight
+  'todayTextColor' => 'var(--bs-primary)',
+  // Border radius for day boxes
+  'boxBorderRadius' => '0.25rem',
+
+  // Border color for the selected day
+  'selectedBorderColor' => 'var(--bs-primary) !important',
+  // Border width for the selected day
+  'selectedBorderWidth' => '2px',
+  // Border color for the hovered day. (css hover selector)
+  'hoverBorderColor' => 'var(--bs-info)',
+  // Border width for the hovered day. (css hover selector)
+  'hoverBorderWidth' => '2px',
 
   // keys for laravel trans helper method (__('key') == trans('key'))
   'localizationKeys' => [
