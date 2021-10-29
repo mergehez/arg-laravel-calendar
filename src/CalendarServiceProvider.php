8941e58e07d6b8ec7bcdb662f586be7ab413c8c1
MergeSoft <argesozdogan@gmail.com>
2021-10-29 23:20:55 +0200

    Added translation file publish option

--- src/CalendarServiceProvider.php
+++ src/CalendarServiceProvider.php
@@ -20,6 +20,16 @@ class CalendarServiceProvider extends ServiceProvider
 
             // php artisan vendor:publish --provider="MergeSoft\CalendarServiceProvider" --tag="component" --force
             $this->publishes([__DIR__ . '/views/arg-calendar.blade.php' => resource_path('views/mergesoft/arg-calendar.blade.php')], 'component');
+
+            
+            $langs = scandir(__DIR__ . "/translations");
+            foreach ($langs as $lang) {
+                if(strpos($lang, ".php") !== false){
+                    $lang = str_replace(".php", "", $lang);
+                    // php artisan vendor:publish --provider="MergeSoft\CalendarServiceProvider" --tag="lang.<lang-code>" --force
+                    $this->publishes([__DIR__ . "/translations/$lang.php" => resource_path("lang/$lang/arg-calendar.php")], "lang.$lang");
+                }
+            }
         }
 
         $this->loadViewsFrom(__DIR__ . '/views', 'arg-calendar');
